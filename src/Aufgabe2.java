6f09631a82cf71f542033baf4244c207b5877ece
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-28 12:30:58 +0100

    Aufgabe3

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -3,14 +3,26 @@ public class Aufgabe2
 
     public static void main(String[] args)
     {
-        for (int i=0; i<3; i+=2) //es wird nur zweimal ausgegebn da wir angegeben haben, dass der Wert nicht öfter als 3Mal ausgegeben werden darf
+        //Aufgabe2
+        //for (int i=0; i<3; i+=2) //es wird nur zweimal ausgegebn da wir angegeben haben, dass der Wert nicht öfter als 3Mal ausgegeben werden darf
+        //{
+        // printName();
+        //}
+        //Aufgabe3
+        for (int i = 0; i < 5; i++)
         {
             printName();
+            printAge(25 + i);
         }
     }
 
     public static void printName()
     {
         System.out.println("Sabrina");
+    }
+
+    public static void printAge(int age)
+    {
+        System.out.println(age);
     }
 }
