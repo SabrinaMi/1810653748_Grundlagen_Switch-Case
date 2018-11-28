470746f46a0d4acb96fa85d3ecb074335558a492
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-28 17:30:14 +0100

    Aufgabe4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args)
+    {
+        Person sabrina = new Person();
+        sabrina.alter = 25;
+        sabrina.name = "Sabrina";
+        sabrina.auge = "blau";
+        sabrina.printPerson();
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,17 @@
+public class Person
+{
+
+        String name;
+        int alter;
+        String auge;
+
+
+    public void printPerson()
+    {
+        System.out.println(name);
+        System.out.println(alter);
+        System.out.println(auge);
+
+    }
+
+}
